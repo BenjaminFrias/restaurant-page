e7153d843850931f79aca8b9986352fc5e45c848
benjaminfrias <benjaminfrias2004@gmail.com>
2024-08-29 15:08:42 -0400

    add js entry point

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { pageLoad } from "./pageLoad";
+export const contentSection = document.querySelector("#content");
+
+const homepageBtn = document.querySelector("#homepage");
+const menuBtn = document.querySelector("#menu");
+const aboutBtn = document.querySelector("#about");
+
+pageLoad();
